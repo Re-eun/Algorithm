67d51f9832385e4c8546c4b542751a5b200e091d
re-eun <149674839+Re-eun@users.noreply.github.com>
2025-03-26 18:40:00 +0900

    Time: 1 ms (64.91%), Space: 59.1 MB (29.72%) - LeetHub

--- LeetCode/Easy/0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.ts
+++ LeetCode/Easy/0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.ts
@@ -1,14 +1,26 @@
 function removeDuplicates(nums: number[]): number {
     // 비교배열에 넣어놓고 같은 값 있으면 삭제
-    const temp = []
-    for (let i = 0; i < nums.length; i++) {
-        if (temp.some((v) => v === nums[i])) {
-            nums.splice(i, 1);
-            i--;
-        } else {
-            temp.push(nums[i])
+    // const temp = []
+    // for (let i = 0; i < nums.length; i++) {
+    //     if (temp.some((v) => v === nums[i])) {
+    //         nums.splice(i, 1);
+    //         i--;
+    //     } else {
+    //         temp.push(nums[i])
+    //     }
+    // }
+    // return nums.length
+
+
+    // 다른 풀이
+    // 이전 인덱스랑 비교해서 같지 않으면 인덱스 1부터 차례로 할당
+    // 처음 원소는 무조건 포함되니 1부터 시작해서 개수 체크 가능
+    let result = 1;
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i] !== nums[i - 1]) {
+            nums[result] = nums[i]
+            result++;
         }
     }
-    return nums.length
-
+    return result
 };
