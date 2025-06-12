cf403b50ef21f8e26a005e2d0f31dca3217a0e25
re-eun <149674839+Re-eun@users.noreply.github.com>
2025-06-12 09:31:08 +0900

    Time: 0 ms (100%), Space: 56.1 MB (19.52%) - LeetHub

--- LeetCode/Easy/0035-search-insert-position/0035-search-insert-position.ts
+++ LeetCode/Easy/0035-search-insert-position/0035-search-insert-position.ts
@@ -1,17 +1,37 @@
 function searchInsert(nums: number[], target: number): number {
-    // 1. target 인덱스 찾기
-    const first = nums.indexOf(target)
-    if (first != -1) return nums.indexOf(target)
+    // // 1. target 인덱스 찾기
+    // const first = nums.indexOf(target)
+    // if (first != -1) return nums.indexOf(target)
 
 
-    // 2. 없다면 들어갈 적절한 인덱스 찾기
-    if (target < nums[0]) return 0
-    for (let i = 0; i < nums.length; i++) {
-        if (target < nums[i-1]) {
-            return i-1
-        } else if (target < nums[i]) {
-            return i
+    // // 2. 없다면 들어갈 적절한 인덱스 찾기
+    // if (target < nums[0]) return 0
+    // for (let i = 0; i < nums.length; i++) {
+    //     if (target < nums[i-1]) {
+    //         return i-1
+    //     } else if (target < nums[i]) {
+    //         return i
+    //     }
+    //     if (i === nums.length - 1) return nums.length
+    // }
+
+         
+    // 삽입될 위치를 찾는 것도 결국 이진탐색의 원리랑 같다.
+    // 왼쪽 값이 오른쪽값과 같거나 작은 한 중간 값을 가져와서 비교하는 과정을 반복, 왼쪽 인덱스 반환
+    let left = 0;
+    let right = nums.length - 1;
+
+    while (left <= right) {
+        let middle = Math.floor((left + right)/2)
+
+        if (target === nums[middle]) {
+            return middle
+        } else if (target < nums[middle]) {
+            right--;
+        } else {
+            left++;
         }
-        if (i === nums.length - 1) return nums.length
     }
+    return left
+
 };
