6bc3cb4f0b7d0f3023ae7b9a2949e9d57b187442
re-eun <149674839+Re-eun@users.noreply.github.com>
2025-07-09 10:15:45 +0900

    Time: 3 ms (56.42%), Space: 65.8 MB (67.39%) - LeetHub

---
+++ LeetCode/Easy/0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.ts
@@ -0,0 +1,17 @@
+function maxProfit(prices: number[]): number {
+    let profit = 0;
+    // 최저가를 기록해두고 차 비교
+    let minPrice = prices[0];
+
+    for (let i = 0; i < prices.length; i++) {
+        if (prices[i] < minPrice) {
+            minPrice = prices[i]
+        }
+        const diff = prices[i] - minPrice
+        if (diff > profit) {
+            profit = diff
+        } 
+    }
+    return profit;
+    
+};
